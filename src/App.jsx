ebfc95f01c6d5895b66120b7f542b0aeda2cf14a
BvienDev <bviendev@gmail.com>
2025-12-12 19:19:04 +0700

    Config mui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
   return (
     <>
       <div>traviss</div>
+
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
